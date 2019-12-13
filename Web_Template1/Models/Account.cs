aff26d7a724f130765df7d0a11a05e81e65927fb
yaowaluk yodpanom <yaowamuz.c@gmail.com>
2019-12-13 23:53:47 +0700

    Add project files.

---
+++ Web_Template1/Models/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_Template1.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+
+        public string AccountNo { get; set; }
+
+        public string IdCard { get; set; }
+
+        public string Title { get; set; }
+        public string Name { get; set; }
+
+
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+       
+        public decimal Capital { get; set; }
+
+        public decimal Coin { get; set; }
+
+        public string Status { get; set; }
+
+        public int Ref_ID { get; set; }
+
+    }
+}
---
+++ Web_Template1/Models/ConnectionDb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Web_Template1.Models
+{
+    public class ConnectionDb :DbContext
+    {
+        // constanct method 
+
+        public ConnectionDb(DbContextOptions<ConnectionDb> options)
+            : base(options)
+        { 
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+
+
+    }
+}
